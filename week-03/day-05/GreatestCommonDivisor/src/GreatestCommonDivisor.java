cc00404d725e742d7316851a8c8bc3d37cd30a8d
Peter Toth <t.peter90@gmail.com>
2020-12-05 15:26:14 +0100

    GreatestCommon Math.abs

--- week-03/day-05/GreatestCommonDivisor/src/GreatestCommonDivisor.java
+++ week-03/day-05/GreatestCommonDivisor/src/GreatestCommonDivisor.java
@@ -2,17 +2,18 @@ public class GreatestCommonDivisor {
     // Find the greatest common divisor of two numbers using recursion.
 
     public static void main(String[] args) {
-        int x = 8;
-        int y = 24;
+        int x = 8896;
+        int y = -24868;
         System.out.println(greatestCommonDivider(x, y, z(x, y)));
 
     }
 
     public static int z(int x, int y) {
-        if (x > y) {
-            return y;
+
+        if (Math.abs(x) > Math.abs(y)) {
+            return Math.abs(y);
         }
-        return x;
+        return Math.abs(x);
     }
 
     public static int greatestCommonDivider(int x, int y, int z) {
